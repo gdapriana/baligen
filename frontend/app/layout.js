443be76b3ac65a88231c875cac916d64f84e1402
gdapriana <gedeapriana36@gmail.com>
2024-06-06 12:04:15 +0800

    frontend

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ['latin'], weight: ['100', '300', '500', '700', '900'] })
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+
+      </body>
     </html>
   );
 }
