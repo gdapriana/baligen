bcf2b78af7733ec562a61c5a43521adc5a3150ef
gdapriana <gedeapriana36@gmail.com>
2024-06-08 12:48:00 +0800

    jwt configuration

--- backend/src/application/app.js
+++ backend/src/application/app.js
@@ -5,9 +5,16 @@ import publicRoute from '../route/public-route.js'
 import authRoute from '../route/auth-route.js'
 import errorMiddleware from '../middleware/error-middleware.js'
 
+const whitelist = ['https://baligen.vercel.app', 'http://localhost:3000']
 const corsOptions = {
-  origin: ['http://baligen.vercel.app', 'http://localhost:3000'],
-  optionsSuccessStatus: 200 // For legacy browser support
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
 }
 
 const app = express()
